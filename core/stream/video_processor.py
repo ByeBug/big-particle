a2958a5eacf921e9f19fc951092475fd51014f78
zhaosiyuan <793773221@qq.com>
2026-06-09 01:53:27 +0800

    更新模型版本为 0.0.8

--- core/stream/video_processor.py
+++ core/stream/video_processor.py
@@ -34,7 +34,7 @@ class VideoStreamProcessor:
         'big_particle': {
             'enabled': True,
             'infer_interval_ms': 100,  # 推理间隔100ms
-            'model_path': '/data/big-particle-data/models/big_particle_v0.0.6',
+            'model_path': '/data/big-particle-data/models/big_particle_v0.0.8',
             'max_batch_size': 8,
         }
     }
